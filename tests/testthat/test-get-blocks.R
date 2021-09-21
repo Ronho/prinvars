39fc03ecb558f5de4f1dd218312da2730fdd35ba
Ron Holzapfel <ronholzapfel@outlook.de>
2021-09-21 11:53:26 +0200

    tests for get blocks

--- tests/testthat/test-get-blocks.R
+++ tests/testthat/test-get-blocks.R
@@ -1,29 +1,100 @@
+
 test_that("get_blocks", {
+  matrix <- matrix(c(
+    1, 1, 0,
+    0, 1, 0,
+    0, 0, 1
+  ), nrow=3, ncol=3)
+
+  expected_result <- list()
+  expected_result[[1]] <- new("Block", features=c(1))
+  expected_result[[2]] <- new("Block", features=c(3))
+  expected_result[[3]] <- new("Block", features=c(2))
   
+  expect_equal(
+    get_blocks(threshold_matrix=matrix, feature_names=c(1:10), check="rows"),
+    expected_result
+  )
+  expect_warning(
+    get_blocks(threshold_matrix=matrix, feature_names=c(1:10), check="rnc")
+  )
 })
 
 test_that("find_combination", {
-  
-})
+  matrix <- matrix(c(
+    1, 1, 0,
+    0, 1, 0,
+    0, 0, 1
+  ), nrow=3, ncol=3)
 
-test_that("are_enough_features", {
-  
-})
-
-test_that("are_enough_eligible_features", {
-  
+  expect_equal(
+    find_combination(
+      threshold_matrix=matrix,
+      eligible_features=c(1:3),
+      ones=1,
+      zeros=2,
+      current_combination=c(),
+      check="rows"
+    ),
+    list(c(1), c(3))
+  )
+  expect_warning(
+    find_combination(
+      threshold_matrix=matrix,
+      eligible_features=c(1:3),
+      ones=1,
+      zeros=2,
+      current_combination=c(),
+      check="rnc"
+    )
+  )
 })
 
 test_that("get_eligible_features", {
-  
+  expect_equal(
+    get_eligible_features(
+      zero_counts=c(1, 2, 3, 2, 4),
+      zeros=2,
+      untaken_features=c(1:3)
+    ),
+    c(2, 3)
+  )
+  expect_equal(
+    get_eligible_features(
+      zero_counts=c(1, 2, 3, 2, 4),
+      zeros=2,
+      untaken_features=c()
+    ),
+    c()
+  )
+})
+
+test_that("are_enough_features", {
+  expect_true(are_enough_features(untaken_features=c(1:5), ones=2))
+  expect_false(are_enough_features(untaken_features=c(1:5), ones=3))
+  expect_true(are_enough_features(untaken_features=c(), ones=0))
+})
+
+test_that("are_enough_eligible_features", {
+  expect_true(
+    are_enough_eligible_features(eligible_features=c(1:5), required_length=3)
+  )
+  expect_false(
+    are_enough_eligible_features(eligible_features=c(), required_length=1)
+  )
+  expect_false(
+    are_enough_eligible_features(eligible_features=c(), required_length=0)
+  )
 })
 
 test_that("check_cols", {
-  
+  expect_equal(check_cols(check="rnc"), TRUE)
+  expect_equal(check_cols(check="rows"), FALSE)
+  expect_error(check_cols(check="Test"))
 })
 
 test_that("err_wrong_check", {
-  
+  expect_error(err_wrong_check(check="Test"))
 })
 
 
