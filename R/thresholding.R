16474b70d66014f3998c7236979915b79195458f
Ron Holzapfel <ronholzapfel@outlook.de>
2021-10-07 22:54:34 +0200

    error for invalid pla

--- R/thresholding.R
+++ R/thresholding.R
@@ -1,6 +1,6 @@
 select_thresholding <- function(eigen_vectors, threshold, mode) {
   valid_threshold(threshold=threshold)
-  eigen_vectors = switch(
+  threshold_matrix = switch(
     tolower(mode),
     "cutoff"=cutoff(x=eigen_vectors, threshold=threshold),
     "percentage"=percentage_per_eigen_vector(
@@ -9,8 +9,20 @@ select_thresholding <- function(eigen_vectors, threshold, mode) {
     ),
     err_wrong_mode(mode=mode)
   )
+  valid_threshold_matrix(threshold_matrix)
 
-  return(eigen_vectors)
+  return(threshold_matrix)
+}
+
+valid_threshold_matrix <- function(threshold_matrix) {
+  row_zeros <- which(get_zero_count(threshold_matrix) == ncol(threshold_matrix))
+  col_zeros <- which(
+    get_zero_count(t(threshold_matrix)) == nrow(threshold_matrix)
+  )
+
+  if(length(row_zeros) > 0 || length(col_zeros) > 0) {
+    err_invalid_pla()
+  }
 }
 
 valid_threshold <- function(threshold) {
@@ -37,6 +49,10 @@ percentage_per_eigen_vector <- function(eigen_vectors, threshold) {
   )
 
   return(eigen_vectors)
+}
+
+err_invalid_pla <- function() {
+  stop("PLA is not valid. Threshold might be too large.")
 }
 
 err_wrong_mode <- function(mode) {
--- tests/testthat/test-thresholding.R
+++ tests/testthat/test-thresholding.R
@@ -1,9 +1,25 @@
-matrix <- matrix(data=seq(0.1, 0.9, by=0.1), nrow=3, ncol=3)
-cutoff_expected_matrix <- matrix(
-    data=c(0, 0, 0, 0, 0, 1, 1, 1, 1), nrow=3, ncol=3
+matrix <- matrix(
+  data=c(
+    0.5, 0.33, 0.2,
+    0.1, 0.36, 0.4,
+    0.9, 0.1, 0.2
+  ), nrow=3, ncol=3
 )
-percentage_expected_matrix <- matrix(data=c(0, 1, 1, 1, 1, 1, 1, 1, 1), nrow=3, ncol=3)
-threshold <- 0.5
+cutoff_expected_matrix <- matrix(
+  data=c(
+    1, 0, 0,
+    0, 1, 1,
+    1, 0, 0
+  ), nrow=3, ncol=3
+)
+percentage_expected_matrix <- matrix(
+  data=c(
+    1, 1, 1,
+    0, 1, 1,
+    1, 0, 0
+  ), nrow=3, ncol=3
+)
+threshold <- 0.35
 
 test_that("select_thresholding", {
   expect_equal(
@@ -42,6 +58,34 @@ test_that("percentage_per_eigen_vector", {
     percentage_per_eigen_vector(eigen_vectors=matrix, threshold=threshold),
     percentage_expected_matrix
   )
+})
+
+test_that("valid_threshold_matrix", {
+  expect_error(
+    valid_threshold_matrix(
+      threshold_matrix=matrix(
+        c(0, 1, 0, 1), nrow=2, ncol=2
+      )
+    )
+  )
+  expect_error(
+    valid_threshold_matrix(
+      threshold_matrix=matrix(
+        c(1, 1, 0, 0), nrow=2, ncol=2
+      )
+    )
+  )
+  expect_silent(
+    valid_threshold_matrix(
+      threshold_matrix=matrix(
+        c(1, 1, 0, 1), nrow=2, ncol=2
+      )
+    )
+  )
+})
+
+test_that("err_invalid_pla", {
+  expect_error(err_invalid_pla())
 })
 
 test_that("err_wrong_mode", {
