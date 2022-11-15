fcc99f2b036a64d1805427d40839d2a47cfc2bc2
JanOBauer <50666196+JanOBauer@users.noreply.github.com>
2022-11-15 08:43:24 +0100

    change order of calculation

--- R/utils.R
+++ R/utils.R
@@ -246,11 +246,12 @@ spla_helper <- function(
   if (criterion != "corrected") {
     W <- eigen$vectors
   }
-
-  eigen$var.all = sum(diag(cov(x))   #Jan 15.11.22                    
+                     
   R <- qr.R(qr(x_P1 %*% W))
-  eigen$values <- (diag(R^2) / (nrow(x) - 1))) / eigen$var.all
   sigma <- cov(x_P1)
+  eigen$var.all = sum(diag(sigma))
+  eigen$values <- (diag(R^2) / (nrow(x) - 1))) / eigen$var.all
+  
   fitting_criteria <- (diag(R^2)/(nrow(x)-1)) / diag(t(W) %*% sigma %*% W)
 
   blocks <- calculate_explained_variance(
